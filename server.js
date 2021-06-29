0910d98a2aa1cc36a1f10f2b1ec04f1a4b3b043c
linhleba <linhleba.it@gmail.com>
2021-06-29 10:04:16 +0700

    update limitter and helmet security

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
-const cookieParser = require("cookie-parser");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+// limit the request to prevent DOS,DDOS
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+const limiter = rateLimit({
+  // 1 hour per window
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
-  })
+  }),
 );
 
 //Routes
 
-app.use("/user", require("./routes/userRouter"));
-app.use("/api", require("./routes/categoryRouter"));
-app.use("/api", require("./routes/uploadImage"));
-app.use("/api", require("./routes/productRouter"));
-app.use("/api", require("./routes/paymentRouter"));
+app.use('/user', require('./routes/userRouter'));
+app.use('/api', require('./routes/categoryRouter'));
+app.use('/api', require('./routes/uploadImage'));
+app.use('/api', require('./routes/productRouter'));
+app.use('/api', require('./routes/paymentRouter'));
 
-app.use("/api", require("./routes/UploadAvatar"));
+app.use('/api', require('./routes/UploadAvatar'));
 
-app.use("/api", require("./routes/commentRouter"));
+app.use('/api', require('./routes/commentRouter'));
 
-app.use("/api", require("./routes/discountRouter"));
-app.use("/api", require("./routes/conversationRouter"));
+app.use('/api', require('./routes/discountRouter'));
+app.use('/api', require('./routes/conversationRouter'));
 
-app.use("/api", require("./routes/messageRouter"));
+app.use('/api', require('./routes/messageRouter'));
 
 //Connect to the database with MongoDB Atlas
 const URI = process.env.MONGODB_URL;
@@ -45,14 +55,14 @@ mongoose.connect(
   },
   (err) => {
     if (err) throw err;
-    console.log("Connected to the database!");
-  }
+    console.log('Connected to the database!');
+  },
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
